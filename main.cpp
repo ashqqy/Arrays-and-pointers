064d83c7dc319ad08365f6d7d474f9ade76cb1ce
ashqqy <Artiom.shaff07@gmail.com>
2024-08-27 15:59:48 +0300

    Version 2
    PrintMassive теперь принимает одномерный массив, функция работает без []

--- main.cpp
+++ main.cpp
@@ -2,30 +2,35 @@
 
 //-------------------------------------------------------------
 
-void PrintMassive (int data[][4]);
+void PrintMassive (int* data, size_t size_y, size_t size_x);
 
 //-------------------------------------------------------------
 
 int main ()
     {
-    int data[5][4] = {{10, 11, 12, 13}, 
-                      {20, 21, 22, 23}, 
-                      {30, 31, 32, 33}, 
-                      {40, 41, 42, 43}, 
-                      {50, 51, 52, 53}};
+    int data[][4] = {{10, 11, 12, 13}, 
+                     {20, 21, 22, 23}, 
+                     {30, 31, 32, 33}, 
+                     {40, 41, 42, 43}, 
+                     {50, 51, 52, 53}};
 
-    PrintMassive (data);
+    size_t size_y = sizeof(data) / sizeof(data[0]);
+    size_t size_x = sizeof(data[0]) / sizeof(data[0][0]);
+    printf ("size_y = %d \n", size_y);
+    printf ("size_x = %d \n", size_x);
+
+    PrintMassive ((int*) data, size_y, size_x);
     }
 
 //-------------------------------------------------------------
 
-void PrintMassive (int data[][4])
+void PrintMassive (int* data, size_t size_y, size_t size_x)
     {
-    for (int y = 0; y < 5; y++)
+    for (int y = 0; y < size_y; y++)
         {
-        for (int x = 0; x < 4; x++)
+        for (int x = 0; x < size_x; x++)
             {
-            printf ("data[%d][%d] = %d \n", y, x, data[y][x]);
+            printf ("data[%d][%d] = %d \n", y, x, *(data + y*size_x + x));
             }
         }
     }
