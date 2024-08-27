96b1bc9c2de2ba7be1ffff92ab7ed88566a6eca5
ashqqy <Artiom.shaff07@gmail.com>
2024-08-27 16:23:49 +0300

    Version 3
    Адрес теперь записан без использования адресной арифметики

--- main.cpp
+++ main.cpp
@@ -3,23 +3,22 @@
 //-------------------------------------------------------------
 
 void PrintMassive (int* data, size_t size_y, size_t size_x);
+void SumMatrix (int *data1, int *data2, size_t size_y, size_t size_x);
 
 //-------------------------------------------------------------
 
 int main ()
     {
-    int data[][4] = {{10, 11, 12, 13}, 
+    int data1[][4] = {{10, 11, 12, 13}, 
                      {20, 21, 22, 23}, 
                      {30, 31, 32, 33}, 
                      {40, 41, 42, 43}, 
                      {50, 51, 52, 53}};
 
-    size_t size_y = sizeof(data) / sizeof(data[0]);
-    size_t size_x = sizeof(data[0]) / sizeof(data[0][0]);
-    printf ("size_y = %d \n", size_y);
-    printf ("size_x = %d \n", size_x);
+    size_t size_y = sizeof(data1) / sizeof(data[0]);
+    size_t size_x = sizeof(data1[0]) / sizeof(data[0][0]);
 
-    PrintMassive ((int*) data, size_y, size_x);
+    PrintMassive ((int*) data1, size_y, size_x);
     }
 
 //-------------------------------------------------------------
@@ -30,7 +29,14 @@ void PrintMassive (int* data, size_t size_y, size_t size_x)
         {
         for (int x = 0; x < size_x; x++)
             {
-            printf ("data[%d][%d] = %d \n", y, x, *(data + y*size_x + x));
+            printf ("data[%d][%d] = %d; ", y, x, *(int*)((size_t)data + y * size_x * sizeof(int) + x * sizeof(int)));
             }
+        printf ("\n");
         }
+    }
+
+//-------------------------------------------------------------
+
+void SumMatrix (int *data1, int *data2, size_t size_y, size_t size_x)
+    {
     }
