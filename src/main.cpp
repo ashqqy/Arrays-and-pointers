3d18a953bfc52c9e57c44a6dcb4df978259c19c3
ashqqy <Artiom.shaff07@gmail.com>
2024-08-28 17:03:05 +0300

    Version 9.1
    Теперь массив с рваными краями печатается используя указатели на начала каждой строки

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 
 //-------------------------------------------------------------
 
-void PrintMassiveNoEdge (int data[], int data_adr[], int n_elements_in_massive);
+void PrintMassiveNoEdge (int data[], int* data_adr[], int data_n_massives);
 
 //-------------------------------------------------------------
 
@@ -18,24 +18,41 @@ int main ()
                   20, 21, 22,
                   30, 31, 32, 33, 34,
                   40, 41, 42, 43};
-    int data_adr[] = {0, 6, 3, 5, 4};
 
-    int n_elements_in_massive = sizeof (data_adr) / sizeof (data_adr[0]) - 1; //первый ноль не считаем
+    int data_len_massives[] = {0, 6, 3, 5, 4}; // длина каждой строки
+    int data_n_massives = sizeof (data_len_massives) / sizeof (data_len_massives[0]); // количество строк
 
-    PrintMassiveNoEdge (data, data_adr, n_elements_in_massive);
+    int data_offset_massives[data_n_massives] = {}; // смещение каждого массива относительно начала даты
+
+    for (int j = 1; j < data_n_massives; j++) // заполняем data_offset_massives
+        {
+        data_offset_massives[j] = data_offset_massives[j-1] + data_len_massives[j];
+        }
+
+    int* data_adr[data_n_massives] = {}; // адреса начала строк и адрес конца последней строки
+    
+    for (int i = 0; i < data_n_massives; i++) // заполняем data_adr
+        {
+        data_adr[i] = data + data_offset_massives[i];
+        }
+
+    // printf ("data[2] = %d; data_adr[2] = %d", data + 14, data_adr[3]);
+    PrintMassiveNoEdge (data, data_adr, data_n_massives);
     }
 
 //-------------------------------------------------------------
 
-void PrintMassiveNoEdge (int data[], int data_adr[], int n_elements_in_massive)
+void PrintMassiveNoEdge (int data[], int* data_adr[], int data_n_massives)
     {
-    for (int i = 0; i < n_elements_in_massive; i++)
+    for (int i = 0; i < data_n_massives - 1; i++)
         {
-        for (int j = 0; j < data_adr[i + 1]; j++)
+        for (int j = 0; j < (int) (data_adr[i + 1] - data_adr[i]); j++) // (int) (data_adr[i + 1] - data_adr[i]) - 
+                                                                        // количество элементов строки, которое считается через 
+                                                                        // разность начала адресов соседних строк
             {
-            printf ("%d ", *(data + data_adr[i] + j));
+            // printf ("%d", (int) ((data_adr[i + 1] - data_adr[i])));
+            printf ("%d ", *(data_adr[i] + j)); // data_adr[i][j]
             }
-        data_adr[i+1] += data_adr[i];
         printf ("\n");
         }
     }
